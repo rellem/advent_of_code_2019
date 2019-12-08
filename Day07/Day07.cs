b0daed32bc6a67ce44da26c1c210f416ccccccd2
Jonas Meller <jonas@meller.nu>
2019-12-08 21:17:59 +0100

    Improve Day07

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using NUnit.Framework;
@@ -32,38 +33,25 @@ namespace Day07
         public static string SolvePart1(string input)
         {
             var maxOutputSignal = 0;
-            for (var p0 = 0; p0 < 5; p0++)
+            foreach (var phaseSettings in GetPermutations(new List<int>() { 0, 1, 2, 3, 4 }, 5))
             {
-                for (var p1 = 0; p1 < 5; p1++)
+                var inputSignal = 0;
+                foreach (var phaseSetting in phaseSettings)
                 {
-                    if (p1 == p0) continue;
-                    for (var p2 = 0; p2 < 5; p2++)
+                    var memory = ParseInput(input);
+                    var machineInput = new BufferBlock<int>();
+                    Trace.Assert(machineInput.Post(phaseSetting));
+                    Trace.Assert(machineInput.Post(inputSignal));
+                    var output = new BufferBlock<int>();
+                    Execute(memory, machineInput, output).GetAwaiter().GetResult();
+                    var outputSignal = output.Receive();
+                    if (outputSignal > maxOutputSignal)
                     {
-                        if (p2 == p1 || p2 == p0) continue;
-                        for (var p3 = 0; p3 < 5; p3++)
-                        {
-                            if (p3 == p2 || p3 == p1 || p3 == p0) continue;
-                            for (var p4 = 0; p4 < 5; p4++)
-                            {
-                                if (p4 == p3 || p4 == p2 || p4 == p1 || p4 == p0) continue;
-                                var inputSignal = 0;
-                                var phaseSettings = new List<int>() { p0, p1, p2, p3, p4 };
-                                foreach (var phaseSetting in phaseSettings)
-                                {
-                                    var memory = parseInput(input);
-                                    var inp = new List<int>() { phaseSetting, inputSignal };
-                                    var output = executePart1(memory, inp);
-                                    var outputSignal = output[output.Count - 1];
-                                    if (outputSignal > maxOutputSignal)
-                                    {
-                                        maxOutputSignal = outputSignal;
-                                    }
-                                    inputSignal = outputSignal;
-                                }
-                            }
-                        }
+                        maxOutputSignal = outputSignal;
                     }
+                    inputSignal = outputSignal;
                 }
+
             }
 
             return maxOutputSignal.ToString();
@@ -71,65 +59,61 @@ namespace Day07
 
         public static string SolvePart2(string input)
         {
-            BufferBlock<int> buffer = new BufferBlock<int>();
             var maxOutputSignal = 0;
-            for (var p0 = 5; p0 < 10; p0++)
+            foreach (var phaseSettings in GetPermutations(new List<int>() { 5, 6, 7, 8, 9 }, 5))
             {
-                for (var p1 = 5; p1 < 10; p1++)
+                var e2a = new BufferBlock<int>();
+                Trace.Assert(e2a.Post(phaseSettings.ElementAt(0)));
+                Trace.Assert(e2a.Post(0));
+
+                var a2b = new BufferBlock<int>();
+                Trace.Assert(a2b.Post(phaseSettings.ElementAt(1)));
+
+                var b2c = new BufferBlock<int>();
+                Trace.Assert(b2c.Post(phaseSettings.ElementAt(2)));
+
+                var c2d = new BufferBlock<int>();
+                Trace.Assert(c2d.Post(phaseSettings.ElementAt(3)));
+
+                var d2e = new BufferBlock<int>();
+                Trace.Assert(d2e.Post(phaseSettings.ElementAt(4)));
+
+                var tasks = new List<Task>
                 {
-                    if (p1 == p0) continue;
-                    for (var p2 = 5; p2 < 10; p2++)
-                    {
-                        if (p2 == p1 || p2 == p0) continue;
-                        for (var p3 = 5; p3 < 10; p3++)
-                        {
-                            if (p3 == p2 || p3 == p1 || p3 == p0) continue;
-                            for (var p4 = 5; p4 < 10; p4++)
-                            {
-                                if (p4 == p3 || p4 == p2 || p4 == p1 || p4 == p0) continue;
-                                var phaseSettings = new List<int>() { p0, p1, p2, p3, p4 };
+                    Execute(ParseInput(input), e2a, a2b),
+                    Execute(ParseInput(input), a2b, b2c),
+                    Execute(ParseInput(input), b2c, c2d),
+                    Execute(ParseInput(input), c2d, d2e),
+                    Execute(ParseInput(input), d2e, e2a)
+                };
 
-                                var e2a = new BufferBlock<int>();
-                                e2a.Post(p0);
-                                e2a.Post(0);
+                Task.WhenAll(tasks).GetAwaiter().GetResult();
 
-                                var a2b = new BufferBlock<int>();
-                                a2b.Post(p1);
+                var outputSignal = e2a.Receive();
 
-                                var b2c = new BufferBlock<int>();
-                                b2c.Post(p2);
-
-                                var c2d = new BufferBlock<int>();
-                                c2d.Post(p3);
-
-                                var d2e = new BufferBlock<int>();
-                                d2e.Post(p4);
-
-                                var tasks = new Task[5];
-                                tasks[0] = executePart2(parseInput(input), e2a, a2b);
-                                tasks[1] = executePart2(parseInput(input), a2b, b2c);
-                                tasks[2] = executePart2(parseInput(input), b2c, c2d);
-                                tasks[3] = executePart2(parseInput(input), c2d, d2e);
-                                tasks[4] = executePart2(parseInput(input), d2e, e2a);
-
-                                Task.WaitAll(tasks);
-
-                                var outputSignal = e2a.Receive();
-
-                                if (outputSignal > maxOutputSignal)
-                                {
-                                    maxOutputSignal = outputSignal;
-                                }
-                            }
-                        }
-                    }
+                if (outputSignal > maxOutputSignal)
+                {
+                    maxOutputSignal = outputSignal;
                 }
             }
 
             return maxOutputSignal.ToString();
         }
 
-        private static int[] parseInput(string input)
+        private static class Instructions
+        {
+            public const int Add = 1;
+            public const int Multiplication = 2;
+            public const int ReadInput = 3;
+            public const int WriteOutput = 4;
+            public const int JumpIfTrue = 5;
+            public const int JumpIfFalse = 6;
+            public const int LessThan = 7;
+            public const int EqualTo = 8;
+            public const int Halt = 99;
+        }
+
+        private static int[] ParseInput(string input)
         {
             var stringOps = input.Trim().Split(",");
             var ops = new int[stringOps.Length];
@@ -142,84 +126,17 @@ namespace Day07
             return ops;
         }
 
-        private static List<int> executePart1(int[] memory, List<int> input)
+        // Taken from: https://stackoverflow.com/a/10629938
+        private static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
         {
-            var inputEnumerator = input.GetEnumerator();
-            var output = new List<int>();
-            var ip = 0;
-            var done = false;
-            while (!done)
-            {
-                var opAndMode = memory[ip];
-                var op = opAndMode % 100;
-                var isImmediateMode1 = ((opAndMode / 100) % 10) > 0;
-                var isImmediateMode2 = ((opAndMode / 1000) % 10) > 0;
-                switch (op)
-                {
-                    case 1:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) + readParam(memory, ip + 2, isImmediateMode2);
-                        ip += 4;
-                        break;
-                    case 2:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) * readParam(memory, ip + 2, isImmediateMode2);
-                        ip += 4;
-                        break;
-                    case 3:
-                        Debug.Assert(inputEnumerator.MoveNext());
-                        memory[memory[ip + 1]] = inputEnumerator.Current;
-                        //Console.WriteLine("read input: " + inputEnumerator.Current);
-                        ip += 2;
-                        break;
-                    case 4:
-                        var paramValue = readParam(memory, ip + 1, isImmediateMode1);
-                        output.Add(paramValue);
-                        //Console.WriteLine("write output: " + paramValue);
-                        ip += 2;
-                        break;
-                    case 5:
-                        if (readParam(memory, ip + 1, isImmediateMode1) > 0)
-                        {
-                            ip = readParam(memory, ip + 2, isImmediateMode2);
-                        }
-                        else
-                        {
-                            ip += 3;
-                        }
-                        break;
-                    case 6:
-                        if (readParam(memory, ip + 1, isImmediateMode1) == 0)
-                        {
-                            ip = readParam(memory, ip + 2, isImmediateMode2);
-                        }
-                        else
-                        {
-                            ip += 3;
-                        }
-                        break;
-                    case 7:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) < readParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
-                        ip += 4;
-                        break;
-                    case 8:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) == readParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
-                        ip += 4;
-                        break;
-                    case 99:
-                        done = true;
-                        break;
-                    default:
-                        throw new Exception("Unexpected opcode: " + op);
-                }
-            }
-
-            inputEnumerator.Dispose();
-
-            return output;
+            if (length == 1) { return list.Select(t => new T[] { t }); }
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
 
-        private static async Task<bool> executePart2(int[] memory, BufferBlock<int> input, BufferBlock<int> output)
+        private static async Task Execute(int[] memory, BufferBlock<int> input, BufferBlock<int> output)
         {
-            //var inputEnumerator = input.GetEnumerator();
             var ip = 0;
             var done = false;
             while (!done)
@@ -230,64 +147,61 @@ namespace Day07
                 var isImmediateMode2 = ((opAndMode / 1000) % 10) > 0;
                 switch (op)
                 {
-                    case 1:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) + readParam(memory, ip + 2, isImmediateMode2);
+                    case Instructions.Add:
+                        memory[memory[ip + 3]] = ReadParam(memory, ip + 1, isImmediateMode1) + ReadParam(memory, ip + 2, isImmediateMode2);
                         ip += 4;
                         break;
-                    case 2:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) * readParam(memory, ip + 2, isImmediateMode2);
+                    case Instructions.Multiplication:
+                        memory[memory[ip + 3]] = ReadParam(memory, ip + 1, isImmediateMode1) * ReadParam(memory, ip + 2, isImmediateMode2);
                         ip += 4;
                         break;
-                    case 3:
+                    case Instructions.ReadInput:
                         memory[memory[ip + 1]] = await input.ReceiveAsync();
                         ip += 2;
                         break;
-                    case 4:
-                        var paramValue = readParam(memory, ip + 1, isImmediateMode1);
-                        output.Post(paramValue);
-                        //Console.WriteLine("write output: " + paramValue);
+                    case Instructions.WriteOutput:
+                        var paramValue = ReadParam(memory, ip + 1, isImmediateMode1);
+                        Trace.Assert(output.Post(paramValue));
                         ip += 2;
                         break;
-                    case 5:
-                        if (readParam(memory, ip + 1, isImmediateMode1) > 0)
+                    case Instructions.JumpIfTrue:
+                        if (ReadParam(memory, ip + 1, isImmediateMode1) > 0)
                         {
-                            ip = readParam(memory, ip + 2, isImmediateMode2);
+                            ip = ReadParam(memory, ip + 2, isImmediateMode2);
                         }
                         else
                         {
                             ip += 3;
                         }
                         break;
-                    case 6:
-                        if (readParam(memory, ip + 1, isImmediateMode1) == 0)
+                    case Instructions.JumpIfFalse:
+                        if (ReadParam(memory, ip + 1, isImmediateMode1) == 0)
                         {
-                            ip = readParam(memory, ip + 2, isImmediateMode2);
+                            ip = ReadParam(memory, ip + 2, isImmediateMode2);
                         }
                         else
                         {
                             ip += 3;
                         }
                         break;
-                    case 7:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) < readParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
+                    case Instructions.LessThan:
+                        memory[memory[ip + 3]] = ReadParam(memory, ip + 1, isImmediateMode1) < ReadParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
                         ip += 4;
                         break;
-                    case 8:
-                        memory[memory[ip + 3]] = readParam(memory, ip + 1, isImmediateMode1) == readParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
+                    case Instructions.EqualTo:
+                        memory[memory[ip + 3]] = ReadParam(memory, ip + 1, isImmediateMode1) == ReadParam(memory, ip + 2, isImmediateMode2) ? 1 : 0;
                         ip += 4;
                         break;
-                    case 99:
+                    case Instructions.Halt:
                         done = true;
                         break;
                     default:
-                        throw new Exception("Unexpected opcode: " + op);
+                        throw new Exception($"Unexpected opcode: {op}");
                 }
             }
-
-            return true;
         }
 
-        private static int readParam(int[] memory, int pos, bool isImmediateMode)
+        private static int ReadParam(int[] memory, int pos, bool isImmediateMode)
         {
             return isImmediateMode ? memory[pos] : memory[memory[pos]];
         }
